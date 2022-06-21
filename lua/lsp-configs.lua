5870e8a3dd4668217b7a9b885651e8797117d597
steve ulrich <sulrich@botwerks.org>
2022-06-20 20:55:04 -0500

    misc updates to lsp-config for linters

--- lua/lsp-configs.lua
+++ lua/lsp-configs.lua
@@ -8,7 +8,10 @@ local null_sources = {
   null_ls.builtins.diagnostics.vale,
   null_ls.builtins.code_actions.gitsigns,
   null_ls.builtins.formatting.black,
-  null_ls.builtins.completion.spell
+  null_ls.builtins.code_actions.shellcheck,
+  null_ls.builtins.diagnostics.pylint,
+  null_ls.builtins.diagnostics.flake8,
+  -- null_ls.builtins.completion.spell
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})

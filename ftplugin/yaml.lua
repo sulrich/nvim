81698e78eb6bc16b510586db0292751805f145d3
steve ulrich <sulrich@arista.com>
2023-04-13 11:19:56 -0700

    fixed ftplugin/yaml migration to lua

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -5,5 +5,4 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.shiftround = true
 vim.opt_local.textwidth = 79
-vim.opt_local.fileformat = unix
 vim.opt_local.expandtab = true

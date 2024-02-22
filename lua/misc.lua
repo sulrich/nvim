5eccf7326c3ba065bd8457217c1fdfb1c7db3184
steve ulrich <sulrich@botwerks.org>
2024-02-22 11:07:52 -0600

    solarized -> non-transparent

--- lua/misc.lua
+++ lua/misc.lua
@@ -18,7 +18,7 @@ end
 -- settings for NeoSolarized
 NeoSolarized.setup {
   style = "dark", -- "dark" or "light"
-  transparent = true, -- true/false; Enable this to disable setting the background color
+  transparent = false, -- true/false; Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   enable_italics = false, -- Italics for different hightlight groups (eg. Statement, Condition, Comment, Include, etc.)
   styles = {

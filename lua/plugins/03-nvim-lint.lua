bff85493a63ddb0e1eb020bb32317db2aee93e8f
steve ulrich <sulrich@botwerks.org>
2024-08-31 00:08:14 -0500

    additional linter tweaks

--- lua/plugins/03-nvim-lint.lua
+++ lua/plugins/03-nvim-lint.lua
@@ -1,9 +1,6 @@
 return {
   "mfussenegger/nvim-lint",
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-  },
+  event = { "BufReadPre", "BufNewFile", },
   config = function()
     local lint = require("lint")
 
@@ -11,5 +8,18 @@ return {
       markdown = { 'markdownlint', 'vale' },
       python = { 'ruff' },
     }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>l", function()
+      lint.try_lint()
+    end, { desc = "trigger linting for the current file" })
   end,
 }

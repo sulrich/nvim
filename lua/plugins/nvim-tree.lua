663aa3f85c0b3905842cb5cd6b748142e617b27b
steve ulrich <sulrich@botwerks.org>
2024-09-25 18:44:59 -0600

    misc. plugin updates, added "<leader>tt" for nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,6 +3,9 @@ return {
     'nvim-tree/nvim-tree.lua',
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { "<leader>tt", "<Cmd>NvimTreeToggle<cr>" },
+    },
     opts = {
       sort = {
         sorter = "case_sensitive",

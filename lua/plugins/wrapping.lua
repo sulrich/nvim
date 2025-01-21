2f18416ae6792db873c471b48a463798ca398e8c
steve ulrich <sulrich@botwerks.org>
2025-01-21 07:17:39 +0000

    initial exploratin of the wrapping.nvin plugin

---
+++ lua/plugins/wrapping.lua
@@ -0,0 +1,15 @@
+return {
+  "andrewferrier/wrapping.nvim",
+  opts = {
+    auto_set_mode_filetype_allowlist = {
+      "asciidoc",
+      "latex",
+      "mail",
+      "markdown",
+      "rst",
+      "tex",
+    }
+  },
+  config = true
+}
+

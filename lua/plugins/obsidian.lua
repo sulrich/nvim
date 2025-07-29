77e11971d64ff74cb1bfc5880260b2262688dd2a
steve ulrich <sulrich@botwerks.org>
2025-07-29 12:15:59 -0500

    obsidian updates

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,4 +1,6 @@
 local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local personal_date_format = "%Y%m%d"
+local current_year = os.date("%Y")
 
 HOME = os.getenv("HOME")
 return {
@@ -36,8 +38,16 @@ return {
           overrides = {
             notes_subdir = "2025",
           },
-        }, -- end workspaces definition
-      },
+        }, -- end: workspace definition
+      }, -- end: workspaces
+      daily_notes = {
+        folder = current_year,
+        date_format = personal_date_format,
+        default_tags = {
+          "daily-notes"
+        },
+        template = "daily-notes.txt",
+      }, -- end: daily_notes config
       -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
       completion = {
         -- enables completion using nvim_cmp
@@ -48,6 +58,19 @@ return {
         min_chars = 4,
       },
       new_notes_location = "notes_subdir",
+      templates = {
+        folder = "templates",
+        date_format = personal_date_format,
+        time_format = "%H:%M",
+        -- A map for custom variables, the key should be the variable and the value a function.
+        -- Functions are called with obsidian.TemplateContext objects as their sole parameter.
+        -- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
+        substitutions = {},
+
+        -- A map for configuring unique directories and paths for specific templates
+        --- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#customizations
+        customizations = {},
+      },
     },
   }
 }

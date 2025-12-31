59c31cffd0bee0066afd6ff72d81ad04abd36d23
steve ulrich <sulrich@botwerks.org>
2025-12-31 13:33:29 -0600

    added remote-nvim.lua

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,38 @@
+-- plugin info: https://github.com/amitds1997/remote-nvim.nvim
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local is_right_host = vim.uv.os_gethostname() == "waffletron"
+
+return {
+  "amitds1997/remote-nvim.nvim",
+  version = "*", -- pin to github releases
+  enabled = is_right_host,
+  dependencies = {
+    "nvim-lua/plenary.nvim", 
+    "MunifTanjim/nui.nvim", -- to build the plugin ui
+    "nvim-telescope/telescope.nvim", -- for picking b/w different remote methods
+  },
+  config = true,
+  opts = {
+    remote = { 
+      copy_dirs = {
+        -- what to copy to remote's neovim config directory
+        config = {
+          dirs = { 
+            "init.lua",
+            "lua",
+            "snippets",
+            "spell",
+            "ftplugin",
+          },
+        },
+      },
+    },
+    -- launch a local neovide instance. this could use some attention.
+    client_callback = function(port, _)
+      local cmd = ("neovide --server localhost:%s"):format(port)
+      vim.fn.jobstart(cmd, {
+        detach = true,
+      })
+    end,
+  },
+}

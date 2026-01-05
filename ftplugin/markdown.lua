17f1524438e947e5ac13004f05d359c5e6e33937
steve ulrich <sulrich@botwerks.org>
2026-01-05 12:27:03 -0600

    misc. ftplugin updates

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,3 +5,8 @@ vim.opt_local.conceallevel= 0
 
 -- enable spell checking
 vim.opt_local.spell = true
+
+-- make sure that this is set explicitly
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
